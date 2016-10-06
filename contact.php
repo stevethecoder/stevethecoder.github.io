8b67b8160b70508855e53c5ea23b998bc7c58131
Steven <steve@littlecreekdesign.com>
2016-10-06 16:51:07 -0400

    new calculator

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php 
-$emailTo = 'steve@littlecreekdesign.com';
-$siteTitle = 'Steven Richards Portfolio';
+$emailTo = 'steve.richards@cbtnuggets.com';
+$siteTitle = 'My Portfolio';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
